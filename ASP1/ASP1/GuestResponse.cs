02bd770f44d44f292bf75b6539f7941ca05b5de9
Алексей Чернышев <silly_lelik@mail.ru>
2020-10-17 15:14:01 +0300

    Basic Web Page

---
+++ ASP1/ASP1/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP1
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Пожалуйста укажите своё имя")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Пожалуйста укажите свою почту")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Пожалуйста укажите свой телефон")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Пожалуйста укажите, придете ли вы")]
+        public bool? WillAttend { get; set; }
+    }
+}
