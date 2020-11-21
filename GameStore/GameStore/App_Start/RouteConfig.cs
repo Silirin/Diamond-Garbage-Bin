7ba8ec4b75edcff79cf9fae109cf2a93a9d65866
Алексей Чернышев <silly_lelik@mail.ru>
2020-11-21 16:24:34 +0300

    Продолжение непонимания

--- GameStore/GameStore/App_Start/RouteConfig.cs
+++ GameStore/GameStore/App_Start/RouteConfig.cs
@@ -11,9 +11,11 @@ namespace GameStore
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapPageRoute(null, "list/{category}/{page}", "~/Pages/Listing.aspx");
             routes.MapPageRoute(null, "list/{page}", "~/Pages/Listing.aspx");
             routes.MapPageRoute(null, "", "~/Pages/Listing.aspx");
             routes.MapPageRoute(null, "list", "~/Pages/Listing.aspx");
+            routes.MapPageRoute("cart", "cart", "~/Pages/CartView.aspx");
         }
     }
 }
--- GameStore/GameStore/Pages/Listing.aspx.cs
+++ GameStore/GameStore/Pages/Listing.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using GameStore.Models;
 using GameStore.Models.Repository;
+using GameStore.Pages.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Routing;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -28,7 +30,8 @@ namespace GameStore.Pages
         {
             get
             {
-                return (int)Math.Ceiling((decimal)repository.Games.Count() / pageSize);
+                int prodCount = FilterGames().Count();
+                return (int)Math.Ceiling((decimal)prodCount / pageSize);
             }
         }
 
@@ -39,14 +42,36 @@ namespace GameStore.Pages
             return reqValue != null && int.TryParse(reqValue, out page) ? page : 1;
         }
 
-        protected IEnumerable<Game> GetGames()
+        public IEnumerable<Game> GetGames()
         {
-            return repository.Games.OrderBy(g => g.GameId).Skip((CurrentPage - 1) * pageSize).Take(pageSize);
+            return FilterGames().OrderBy(g => g.GameId).Skip((CurrentPage - 1) * pageSize).Take(pageSize);
+        }
+
+        private IEnumerable<Game> FilterGames()
+        {
+            IEnumerable<Game> games = repository.Games;
+            string currentCategory = (string)RouteData.Values["category"] ?? Request.QueryString["category"];
+            return currentCategory == null ? games : games.Where(p => p.Category == currentCategory);
         }
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack)
+            {
+                int selectedGameId;
+                if (int.TryParse(Request.Form["add"], out selectedGameId))
+                {
+                    Game selectedGame = repository.Games.Where(g => g.GameId == selectedGameId).FirstOrDefault();
 
+                    if (selectedGame != null)
+                    {
+                        SessionHelper.GetCart(Session).AddItem(selectedGame, 1);
+                        SessionHelper.Set(Session, SessionKey.RETURN_URL, Request.RawUrl);
+
+                        Response.Redirect(RouteTable.Routes.GetVirtualPath(null, "cart", null).VirtualPath);
+                    }
+                }
+            }
         }
     }
 }
